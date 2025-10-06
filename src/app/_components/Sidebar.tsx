b68c5ddf95810eb254748308b659182d4ce1c5d9
hatakiii <25lp5598@nest.edu.mn>
2025-10-06 23:32:30 +0800

    small changes

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+
 export const Sidebar = () => {
-  return <div className="bg-red-500 w-1/6 h-screen">Sidebar</div>;
+  return (
+    <div className="bg-red-500 w-1/6 h-screen">
+      Sidebar
+      <div>
+        <Image
+          src="public/NomNomLogo.png"
+          alt="main logo"
+          width={40}
+          height={40}
+        />
+        <span>Nom Nom</span>
+        <span>Swift delivery</span>
+      </div>
+    </div>
+  );
 };
