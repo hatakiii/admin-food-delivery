939232e08055f28e5c35e20eecd22d9d52c60f7a
hatakiii <25lp5598@nest.edu.mn>
2025-10-07 16:07:51 +0800

    create food dialog

---
+++ src/app/_components/CreateFoodDialog.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+export const CreateFoodDialog = () => {
+  const [name, setName] = useState<string>("");
+  const [price, setPrice] = useState<number>(0);
+
+  const addFoodHandler = () => {};
+
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant={"outline"}>Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
